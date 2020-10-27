89cb91a629a017d48400c27ee11ce3df51852879
Raton Biswas <raton.biswas005@gmail.com>
2020-10-27 14:21:43 +0600

    fix error|result route|done successfully👌

--- Movify/app.js
+++ Movify/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const chalk = require('chalk')
 const path = require('path')
-const searchRoute = require('./router/searchRoute.js')
+const searchRoute = require('./router/searchRoute')
 
 
 
@@ -21,10 +21,6 @@ app.use(express.static('public'))
 app.use(express.json());
 
 app.use('/movie',searchRoute)
-
-// app.get('/search',(req,res) => {
-//     res.render('search')
-// })
 
 
 
--- Movify/controllers/searchController.js
+++ Movify/controllers/searchController.js
@@ -1,18 +1,20 @@
 const request = require('request')
 
-
 exports.search = (req,res) => {
-    res.render('search');
-  };
+  res.render('search');
+};
 
 exports.results = (req,res) => {
   let query = req.query.search;
-  console.log(query);
-  request('https://api.themoviedb.org/3/search/movie?api_key=311bb5d1501921bd7dea7220d6d42c27&query='+query, (err,response,body)=>{
-    if(err){
-      console.log(err);
-    }
-    let data = JSON.parse(body);
-    res.render('movies',{data:data,searchQuery:query});
-  })
-}
+  // console.log(query);
+    
+    request('https://api.themoviedb.org/3/search/movie?api_key=311bb5d1501921bd7dea7220d6d42c27&query='+query, (error,response,body)=>{
+      if(error){
+        console.log(error);
+      }
+      let data = JSON.parse(body);
+      // console.log(data);
+      res.render('movies',{data:data,searchQuery:query});
+    })
+  }
+
