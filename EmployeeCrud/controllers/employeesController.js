13d93bf74e44e2920c40b274cc50a3d5c249dd61
Raton Biswas <raton.biswas005@gmail.com>
2020-10-28 22:06:26 +0600

    add new employee route route|design 👍

--- EmployeeCrud/controllers/employeesController.js
+++ EmployeeCrud/controllers/employeesController.js
@@ -1,3 +1,6 @@
 export const getone = (req, res) => {
     res.render('index')
 }
+export const getnew = (req, res) => {
+    res.render('new')
+}
--- EmployeeCrud/routes/employeesRoute.js
+++ EmployeeCrud/routes/employeesRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 import * as employeesController from '../controllers/employeesController.js'
 
 router.get('/',employeesController.getone)
+router.get('/new',employeesController.getnew)
+
 
 export default router
