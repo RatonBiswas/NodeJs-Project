4ae91db39e3375241e188b1f9e62a7a13870af91
Raton Biswas <raton.biswas005@gmail.com>
2020-10-30 14:24:09 +0600

    deploy heroku👌

--- EmployeeCrud/routes/employeesRoute.js
+++ EmployeeCrud/routes/employeesRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import * as employeesController from '../controllers/employeesController.js'
 
-router.get('',employeesController.getone)
+router.get('/',employeesController.getone)
 router.get('/new',employeesController.getnew)
 router.post('/new',employeesController.createEmployee)
 router.get('/search',employeesController.getSearch)
