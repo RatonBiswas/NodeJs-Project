251fa1109ad50caa75b83b36f22daa5e1d8c47b6
Raton Biswas <raton.biswas005@gmail.com>
2020-10-30 14:00:15 +0600

    add success|error messages|output img🤷‍♀️🤷‍♀️

--- EmployeeCrud/controllers/employeesController.js
+++ EmployeeCrud/controllers/employeesController.js
@@ -3,7 +3,8 @@ export const getone = (req, res) => {
     Employee.find({}).then((employees) =>{
         res.render('index',{employees:employees});
     }).catch((err) => {
-        console.log(err);
+        req.flash('error_msg','Error: '+err)
+        res.redirect('/employee')
     })
 }
 export const getnew = (req, res) => {
@@ -17,9 +18,11 @@ export const createEmployee = (req, res) => {
         salary: req.body.salary
     };
     Employee.create(newEmployee).then((employee) =>{
+        req.flash('success_msg','Employee data added to database successfully.')
         res.redirect('/employee')
     }).catch((err) =>{
-        console.log(err);
+        req.flash('error_msg','Error: '+err)
+        res.redirect('/employee')
     })
 }
 
@@ -32,7 +35,8 @@ export const getEmployeeInfo = (req, res) => {
     Employee.findOne(searchQuery).then((employee) => {
         res.render('search',{employee:employee})
     }).catch((err) => {
-        console.log(err);
+        req.flash('error_msg','Error: '+err)
+        res.redirect('/employee')
     })
 }
 
@@ -41,7 +45,8 @@ export const getUpdateView = (req, res) => {
     Employee.findOne(searchQuery).then((employee) => {
         res.render('edit',{employee:employee})
     }).catch((err) => {
-        console.log(err);
+        req.flash('error_msg','Error: '+err)
+        res.redirect('/employee')
     })
 }
 
@@ -52,17 +57,21 @@ export const getUpdate = (req, res) => {
         designation: req.body.designation,
         salary: req.body.salary
     }}).then((employee) =>{
+        req.flash('success_msg','Employee data updated successfully.')
         res.redirect('/employee')
     }).catch((err) =>{
-        console.log(err);
+        req.flash('error_msg','Error: '+err)
+        res.redirect('/employee')
     })
 }
 
 export const deleteOne = (req, res) => {
     let searchQuery = {_id:req.params.id}
     Employee.findByIdAndDelete(searchQuery).then((employee) =>{
+        req.flash('success_msg','Employee deleted successfully.')
         res.redirect('/employee')
     }).catch((err) =>{
-        console.log(err);
+        req.flash('error_msg','Error: '+err)
+        res.redirect('/employee')
     })
 }
