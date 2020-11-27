a08431623df0146187dbeefa24faf57a877ae418
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-11-27 23:09:55 +0600

    Connect socket io ✌

--- Real-time-chat/app.js
+++ Real-time-chat/app.js
@@ -1,21 +1,41 @@
-import express from "express";
-import path from "path";
-import chalk from "chalk";
-const app = express();
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+const  express = require('express')
+const path =require('path')
+const chalk = require('chalk')
+const  socketio =require('socket.io');
+// import * as socketio from 'socket.io-client';
+// import {socketio} from "/socket.io/socket.io.js"
+const http = require('http')
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const server = http.createServer(app)
+// const io = socketio(server)
+const io = socketio(server)
+
+// Run when client connects
+io.on('connection', socket => {
+  console.log('New WS connection!...');
+  socket.emit('message','Welcome to ChatBoard')
+})
+// io.on('connect', socket => {
+//   console.log("New WS connection.....");
+// });
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 // view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+//set static folder
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () =>
+server.listen(port, () =>
   console.log(`app running on port ${chalk.greenBright(port)} ....`),
 );
