0a9399e2705da3d8b68f681de5a0bedc1914d307
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-01-16 22:36:37 +0600

    contacts model

---
+++ PostBlogger/Contacts.js
@@ -0,0 +1,39 @@
+class Contacts {
+    constructor(){
+        this.contacts =[]
+    }
+    // get all contacts
+    getAllContacts(){
+        return this.contacts
+    }
+    // get single contact
+    getConstactsById(id){
+        return this.contacts.find(contacts => contacts.id === id)
+    }
+    // create contacts
+    createContact(contact){
+        contact.id = this.contacts.length + 1
+        this.contacts.push(contact)
+        return contact 
+    }
+    // update contact by id
+    updateContactById(id, updateContact){
+        let index = this.contacts.findIndex(contact => contact.id === id)
+
+        this.contacts[index].name = updateContact.name || this.contacts[index].name
+        this.contacts[index].phone = updateContact.phone || this.contacts[index].phone
+        this.contacts[index].email = updateContact.email || this.contacts[index].email
+
+        return this.contacts[index]
+    }
+    // delete contact by id    
+    deleteContactById(id) {
+        let index = this.contacts.findIndex(contact => contact.id === id)
+        let deleteObject = this.contacts[index]
+        this.contacts = this.contacts.filter(contact => contact.id !== id)
+
+        return deleteObject
+    }
+}
+
+exports.contacts = new Contacts()
