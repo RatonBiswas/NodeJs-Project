accb3c1f345319478b97d41ef964c40593051732
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-01-18 23:20:48 +0600

    Post model ✌

---
+++ Article_Story/models/Post.js
@@ -0,0 +1,41 @@
+// title , body , author ,tags, thumnail , readtime , likes, dislikes, comments
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        trim: true,
+        maxLength:100,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true,
+    },
+    author:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags:{
+        type:[String],
+        required:true
+    },
+    thumnail: String,
+    readTime: String,
+    likes: [mongoose.Schema.ObjectId],
+    dislikes: [mongoose.Schema.ObjectId],
+    comments: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Comment',
+        }
+    ]
+}, {
+    timestamp:true
+})
+
+
+const Post = mongoose.model('Post',postSchema)
+
+module.exports = Post
