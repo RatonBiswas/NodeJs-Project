a8881fd5b1f10be871c956d3bacaa1517673de8f
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-01-18 23:11:49 +0600

    profile model

---
+++ Article_Story/models/Profile.js
@@ -0,0 +1,47 @@
+// User, title, bio , profilePics , Links : {fb, tw, }, posts , bookmarks
+
+const mongoose = require('mongoose')
+
+const profileSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title:{
+        type:String,
+        trim: true,
+        maxLength:100,
+    },
+    bio:{
+        type:String,
+        trim: true,
+        maxLength:500,
+    },
+    profilePic:String,
+    links:{
+        website:String,
+        facebook:String,
+        twitter:String,
+        github:String,
+    },
+    posts:[
+        {
+            type:mongoose.Schema.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    bookmarks:[
+        {
+            type:mongoose.Schema.ObjectId,
+            ref: 'Post'
+        }
+    ]
+    
+}, {
+    timestamp:true
+})
+
+const Profile = mongoose.model('Profile',profileSchema)
+
+module.exports = Profile
