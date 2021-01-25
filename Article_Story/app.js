08958604409a556b08172f77eeba913ae2673f8a
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-01-25 22:30:16 +0600

    Setup Partials

--- Article_Story/app.js
+++ Article_Story/app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 
-
-
-
-
 const app = express()
+ 
+
 
 // setup view engine
 app.set('view engine','ejs')
@@ -21,12 +19,12 @@ const middleware = [
 app.use(middleware)
 
 app.get('/', (req, res) =>{
+    res.render('pages/auth/signup',{title: 'create a new account'})
     res.json('Hello User')
 })
 
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
 })
