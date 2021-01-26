a92b2c23be68520c3dbe1328d43b11afafe7beed
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-01-26 16:45:23 +0600

    login controller

--- Article_Story/controllers/authController.js
+++ Article_Story/controllers/authController.js
@@ -15,7 +15,7 @@ exports.signupPostController = async (req, res, next) => {
     });
 
     let createdUser = await user.save();
-    console.log("Successfully Created Account", createdUser);
+    console.log("Successfully Created Account");
     // console.log('Successfully Created Account', user);
     res.render("pages/auth/signup", { title: "Create A New Account" });
   } catch (e) {
@@ -27,6 +27,29 @@ exports.signupPostController = async (req, res, next) => {
 exports.loginGetController = (req, res, next) => {
   res.render("pages/auth/login", { title: "Log In Your Account" });
 };
-exports.loginPostController = (req, res, next) => {};
+exports.loginPostController = async(req, res, next) => {
+    let {email, password} = req.body
+    try{
+        let user = await User.findOne({ email})
+        if(!user) {
+            return res.json({
+                message: "Invalid credential"
+            })
+        }
+
+
+        let match = await bcrypt.compare(password, user.password)
+        if(!match) {
+            return res.json({
+                message: "Invalid credential"
+            })
+        }
+        console.log("Login successful");
+        res.render("pages/auth/login", { title: "Log In Your Account" });
+    }catch(e){
+        console.log(e);
+        next(e)
+    }
+};
 
 exports.logoutController = (req, res, next) => {};
