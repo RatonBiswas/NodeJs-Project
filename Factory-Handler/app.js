2da44d0db3e2c0d152d03caaa623455e9da57e74
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-02-07 13:46:33 +0600

    find some error

--- Factory-Handler/app.js
+++ Factory-Handler/app.js
@@ -1,5 +1,5 @@
-const AppError = require('./../utils/appError');
-const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./utils/appError')
+const catchAsync = require('./utils/catchAsync')
 
 exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
